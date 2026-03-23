cda8d1d239a34e403bcc3f2147a27b67b389b087
kmechler@uni-mannheim.de <ma_kmechler@uc3n010.localdomain>
2026-03-23 16:53:01 +0100

    added test for llm call

--- test.py
+++ test.py
@@ -1,2 +1,32 @@
-print("it works")
-print("and again")
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+model_id = "meta-llama/Llama-3.2-3B-Instruct"
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    torch_dtype = "auto",
+    device_map = "auto"
+)
+
+messages = [
+    {"role": "system", "content": "You are a helpful assistant who keeps their answers clear and concise."},
+    {"role": "user", "content": "write me a poem on knowledge graphs"}
+]
+
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize = False,
+    add_generation_prompt = True
+)
+
+inputs = tokenizer(text, return_tensors = "pt").to(model.device)
+
+with torch.no_grad():
+    outputs = model.generate(
+        **inputs,
+        max_new_tokens=200,
+        temperature = 0
+    )
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
